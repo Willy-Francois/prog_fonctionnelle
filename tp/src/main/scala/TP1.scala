57d91176f759f70e402540b7e03c1cb5c1091add
Willy Francois <willy.francois@etu.umontpellier.fr>
2022-02-03 14:11:52 +0100

    tp1 FINISH

--- tp/src/main/scala/TP1.scala
+++ tp/src/main/scala/TP1.scala
@@ -2,41 +2,91 @@ object TP1Ex1:
 
   /* Définissez les opérateurs booléens suivants en utilisant des expressions conditionnelles
    * et les valeurs 'true' et 'false' */
-  def not(b: Boolean): Boolean = ???
+  def not(b: Boolean): Boolean = !b
 
-  def and(l: Boolean, r: Boolean): Boolean = ???
+  def and(l: Boolean, r: Boolean): Boolean = {
+    if (l && r){
+      true
+    }else {
+      false
+    }
+  }
 
-  def or(l: Boolean, r: Boolean): Boolean = ???
+  def or(l: Boolean, r: Boolean): Boolean = {
+    if (!l && !r){
+      false
+    }else {
+      true
+    }
+  }
 
-  def xor(l: Boolean, r: Boolean): Boolean = ???
+  def xor(l: Boolean, r: Boolean): Boolean = {
+    if (l && r){
+      false
+    }else if (!l && !r){
+      false
+    }else{
+      true
+    }
+  }
 
   /* Définissez la valeur absolue d'un nombre */
-  def abs(x: Double): Double = ???
+  def abs(x: Double): Double =
+    if x<0 then
+      x*(-1)
+    else
+      x
+
+
 
   /* Définissez la factorielle d'un nombre positif à l'aide d'une fonction récursive */
   def factorial(n: BigInt): BigInt =
     if n < 0 then
       throw new IllegalArgumentException("negative integer")
+    else if n==1 then
+      1
     else
-      ???
+      n*factorial(n-1)
 
   /* Définissez l'exponentiation à l'aide d'une fonction récursive */
   def power(b: BigInt, n: Int): BigInt =
     if n < 0 then
       throw new IllegalArgumentException("negative integer")
+    else if n==0 then
+        1
     else
-      ???
+      b*power(b, n-1)
 
   /* Définissez une fonction récursive qui calcule la somme des entiers de n à m (inclus). */
-  def sumSegment(n: Int, m: Int): Int = ???
+  def sumSegment(n: Int, m: Int): Int =
+    if n==m then
+      n
+    else if n<m then
+      n+sumSegment(n+1, m)
+    else 0
+
+
 
   /* Définissez une fonction non-récursive qui effectue le même calcul.
    *
    * Indication: utilisez la formule pour calculer la somme des n premiers entiers: n * (n + 1) / 2 */
-  def fastSumSegment(n: Int, m: Int): Int = ???
+  def fastSumSegment(n: Int, m: Int): Int =
+        (m * (m + 1) / 2) - ((n-1) * n / 2)
 
   /* Définissez une fonction récursive qui calcule la représentation en binaire d'un entier n */
-  def binaryString(n: Int): String = ???
+  def binaryString(n: Int): String =
+    if n<0 then
+      binaryString(n*(-1))+"-"
+    if n == 1 then
+      return "1"
+    if n==0 then
+      return "0"
+    if n%2==0 then
+      binaryString(n/2)+"0"
+    else
+      binaryString(n/2)+"1"
+
+
 
   /* Définissez une fonction qui calcule le PGCD de deux nombres, en vous basant sur l'algorithme d'Euclide */
   def gcd(a: Int, b: Int): Int = ???
@@ -63,4 +113,3 @@ object TP1Ex1:
     println(s"the binary representation of ${n4} is ${binaryString(n4)}")
     val a = 64
     val b = 24
-    println(s"the greatest commond divisor of ${a} and ${b} is ${gcd(a, b)}")
